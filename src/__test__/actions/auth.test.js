5f8a6e53333bf75d8847340bb74b704c1fdad6e0
Fan Yang <yangfcm16@gmail.com>
2020-08-11 15:49:12 +0930

    test for auth action

--- src/__test__/actions/auth.test.js
+++ src/__test__/actions/auth.test.js
@@ -1,14 +1,60 @@
+import configMockStore from "redux-mock-store";
+import thunk from "redux-thunk";
+import { defAxios as axios } from "settings";
 import { SIGN_IN, SIGN_OUT, CATCH_ERROR } from "actions/types";
 // import { defAxios as axios } from "settings";
 import { signIn, signOut } from "actions/auth";
+import { userTokenData, userInfoData } from "../fixtures/auth";
 
-test.todo("signIn action should work");
+const mockStore = configMockStore([thunk]);
+describe("Test auth action", () => {
+  let store;
+  beforeEach(() => {
+    store = mockStore();
+  });
 
-test("signOut action should remove access token and return object", () => {
-  const action = signOut();
-  const accessToken = localStorage.getItem("access_token");
-  expect(accessToken).toBeFalsy();
-  expect(action).toEqual({
-    type: SIGN_OUT,
+  it("signIn action should work", async (done) => {
+    axios.get.mockResolvedValue({
+      data: userInfoData,
+    });
+    await store.dispatch(signIn(userTokenData));
+    const accessToken = localStorage.getItem("access_token");
+    expect(accessToken).toBe(
+      `${userTokenData.wc.token_type} ${userTokenData.wc.access_token}`
+    );
+    expect(axios.get).toHaveBeenCalledWith(
+      "https://www.googleapis.com/oauth2/v1/userinfo?alt=json",
+      {
+        headers: {
+          Authorization: accessToken,
+        },
+      }
+    );
+    expect(store.getActions()[0]).toEqual({
+      type: SIGN_IN,
+      payload: {
+        email: userInfoData.email,
+        username: userInfoData.name,
+        lastName: userInfoData.family_name,
+        firstName: userInfoData.given_name,
+        avatar: userInfoData.picture,
+      },
+    });
+    done();
+  });
+
+  it("signOut action should remove access token and return object", () => {
+    let accessToken;
+    const mockAccessToken = "mock_access_token";
+    localStorage.setItem("access_token", mockAccessToken);
+    accessToken = localStorage.getItem("access_token");
+    expect(accessToken).toBeTruthy();
+
+    const action = signOut();
+    accessToken = localStorage.getItem("access_token");
+    expect(accessToken).toBeFalsy();
+    expect(action).toEqual({
+      type: SIGN_OUT,
+    });
   });
 });
---
+++ src/__test__/fixtures/auth.js
@@ -0,0 +1,18 @@
+export const userTokenData = {
+  wc: {
+    token_type: "Bearer",
+    access_token: "mock_access_token",
+  },
+};
+
+export const userInfoData = {
+  id: "1234567",
+  email: "test@gmail.com",
+  verified_email: true,
+  name: "Test Test",
+  given_name: "Test",
+  family_name: "Test",
+  picture:
+    "https://lh3.googleusercontent.com/a-/AOh14GgHZ_IO2RUfg770gLs9V6UOsJaDMsWmGKP4AUna",
+  locale: "zh-CN",
+};
