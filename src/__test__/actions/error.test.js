aeee23e3bb35e3775e18b882474005b1aa81632c
Fan Yang <yangfcm16@gmail.com>
2022-10-10 21:50:30 +1030

    Reset project.

--- src/__test__/actions/error.test.js
+++
@@ -1,9 +0,0 @@
-import { clearError } from "actions/error";
-import { CLEAR_ERROR } from "actions/types";
-
-it("should setup clearError action object", () => {
-  const action = clearError();
-  expect(action).toEqual({
-    type: CLEAR_ERROR,
-  });
-});
