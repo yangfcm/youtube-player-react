ea0e955676491341e6d780fe11d67d3e10c03e2d
Fan Yang <yangfcm16@gmail.com>
2023-01-18 00:24:00 +1030

    Make google signin work.

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,3 +1,36 @@
+import { useEffect } from "react";
+import { GoogleLogin } from "react-google-login";
+import { gapi } from "gapi-script";
+
 export function Home() {
-  return <>Home page</>;
+  useEffect(() => {
+    const initClient = () => {
+      gapi.client.init({
+        clientId: process.env.REACT_APP_CLIENT_ID,
+        scope: "openid ",
+      });
+    };
+    gapi.load("client:auth2", initClient);
+  }, []);
+
+  const onSuccess = (res: any) => {
+    console.log("success:", res);
+  };
+  const onFailure = (err: any) => {
+    console.log("failed:", err);
+  };
+
+  return (
+    <>
+      Home page{" "}
+      <GoogleLogin
+        clientId={process.env.REACT_APP_CLIENT_ID || ""}
+        buttonText="Sign in with Google"
+        onSuccess={onSuccess}
+        onFailure={onFailure}
+        cookiePolicy={"single_host_origin"}
+        isSignedIn={true}
+      />
+    </>
+  );
 }
