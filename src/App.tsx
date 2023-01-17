82f2cac7a31cafe469ab8406bc9c4b89d96a9dab
Fan Yang <yangfcm16@gmail.com>
2023-01-17 22:01:59 +1030

    Add router.

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import { Provider } from "react-redux";
+import { BrowserRouter } from "react-router-dom";
 import CssBaseline from "@mui/material/CssBaseline";
 import { createTheme, ThemeProvider } from "@mui/material/styles";
 import AppBar from "@mui/material/AppBar";
@@ -11,6 +12,7 @@ import IconButton from "@mui/material/IconButton";
 import Box from "@mui/material/Box";
 import { store } from "./app/store";
 import { SidebarMenu } from "./components/SidebarMenu";
+import { Router } from "./Router";
 
 const mdTheme = createTheme();
 
@@ -21,87 +23,89 @@ function App() {
 
   return (
     <Provider store={store}>
-      <ThemeProvider theme={mdTheme}>
-        <CssBaseline />
-        <Box sx={{ display: " flex", height: "100vh" }}>
-          <AppBar
-            position="fixed"
-            sx={{
-              zIndex: {
-                md: mdTheme.zIndex.drawer + 1,
-              },
-            }}
-          >
-            <Toolbar>
-              <IconButton
-                edge="start"
-                color="inherit"
-                onClick={(e) => {
-                  setOpen(true);
-                }}
-                sx={{
-                  display: {
-                    xs: "inline-flex",
-                    md: "none",
-                  },
-                }}
-              >
-                <MenuIcon />
-              </IconButton>
-              <Typography
-                component="h1"
-                variant="h6"
-                color="inherit"
-                noWrap
-                sx={{ flexGrow: 1 }}
-              >
-                Youtube
-              </Typography>
-            </Toolbar>
-          </AppBar>
-          <Drawer
-            open={open}
-            onClick={() => {
-              setOpen(false);
-            }}
-            sx={{
-              display: {
-                xs: "block",
-                md: "none",
-              },
-              "& .MuiDrawer-paper": {
-                boxSizing: "border-box",
+      <BrowserRouter>
+        {" "}
+        <ThemeProvider theme={mdTheme}>
+          <CssBaseline />
+          <Box sx={{ display: " flex", height: "100vh" }}>
+            <AppBar
+              position="fixed"
+              sx={{
+                zIndex: {
+                  md: mdTheme.zIndex.drawer + 1,
+                },
+              }}
+            >
+              <Toolbar>
+                <IconButton
+                  edge="start"
+                  color="inherit"
+                  onClick={(e) => {
+                    setOpen(true);
+                  }}
+                  sx={{
+                    display: {
+                      xs: "inline-flex",
+                      md: "none",
+                    },
+                  }}
+                >
+                  <MenuIcon />
+                </IconButton>
+                <Typography
+                  component="h1"
+                  variant="h6"
+                  color="inherit"
+                  noWrap
+                  sx={{ flexGrow: 1 }}
+                >
+                  Youtube
+                </Typography>
+              </Toolbar>
+            </AppBar>
+            <Drawer
+              open={open}
+              onClick={() => {
+                setOpen(false);
+              }}
+              sx={{
+                display: {
+                  xs: "block",
+                  md: "none",
+                },
+                "& .MuiDrawer-paper": {
+                  boxSizing: "border-box",
+                  width: drawerWidth,
+                },
+              }}
+            >
+              <SidebarMenu />
+            </Drawer>
+            <Drawer
+              variant="permanent"
+              open
+              sx={{
+                display: {
+                  xs: "none",
+                  md: "block",
+                },
+                "& .MuiDrawer-paper": {
+                  boxSizing: "border-box",
+                  width: drawerWidth,
+                },
                 width: drawerWidth,
-              },
-            }}
-          >
-            <SidebarMenu />
-          </Drawer>
-          <Drawer
-            variant="permanent"
-            open
-            sx={{
-              display: {
-                xs: "none",
-                md: "block",
-              },
-              "& .MuiDrawer-paper": {
-                boxSizing: "border-box",
-                width: drawerWidth,
-              },
-              width: drawerWidth,
-            }}
-          >
-            <Toolbar />
-            <SidebarMenu />
-          </Drawer>
-          <Box component="main" sx={{ flexGrow: 1, p: 2 }}>
-            <Toolbar />
-            <div></div>
-            main content
+              }}
+            >
+              <Toolbar />
+              <SidebarMenu />
+            </Drawer>
+            <Box component="main" sx={{ flexGrow: 1, p: 2 }}>
+              <Toolbar />
+              <Router />
+            </Box>
           </Box>
-        </Box>
-      </ThemeProvider>
+        </ThemeProvider>
+      </BrowserRouter>
     </Provider>
   );
 }
--- src/components/SidebarMenu.tsx
+++ src/components/SidebarMenu.tsx
@@ -1,30 +1,36 @@
+import { Link } from "react-router-dom";
 import List from "@mui/material/List";
 import ListItem from "@mui/material/ListItem";
 import ListItemButton from "@mui/material/ListItemButton";
 import ListItemIcon from "@mui/material/ListItemIcon";
 import ListItemText from "@mui/material/ListItemText";
+import MuiLink from "@mui/material/Link";
 import SubscriptionsIcon from "@mui/icons-material/Subscriptions";
 import HomeIcon from "@mui/icons-material/Home";
 
 export function SidebarMenu() {
   return (
     <List>
-      <ListItem disablePadding>
-        <ListItemButton>
-          <ListItemIcon>
-            <HomeIcon />
-          </ListItemIcon>
-          <ListItemText primary="Home" />
-        </ListItemButton>
-      </ListItem>
-      <ListItem disablePadding>
-        <ListItemButton>
-          <ListItemIcon>
-            <SubscriptionsIcon />
-          </ListItemIcon>
-          <ListItemText primary="Subscriptions" />
-        </ListItemButton>
-      </ListItem>
+      <MuiLink component={Link} to="/" underline="none">
+        <ListItem disablePadding>
+          <ListItemButton>
+            <ListItemIcon>
+              <HomeIcon />
+            </ListItemIcon>
+            <ListItemText primary="Home" />
+          </ListItemButton>
+        </ListItem>
+      </MuiLink>
+      <MuiLink component={Link} to="/subscriptions" underline="none">
+        <ListItem disablePadding>
+          <ListItemButton>
+            <ListItemIcon>
+              <SubscriptionsIcon />
+            </ListItemIcon>
+            <ListItemText primary="Subscriptions" />
+          </ListItemButton>
+        </ListItem>
+      </MuiLink>
     </List>
   );
 }
