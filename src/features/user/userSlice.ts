e191f72105cefffd37f8a54d26bce26affb35ebe
Fan Yang <yangfcm16@gmail.com>
2023-01-18 15:04:40 +1030

    Add user reducer.

--- src/features/user/userSlice.ts
+++ src/features/user/userSlice.ts
@@ -1,17 +1,38 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+import { RootState } from "../../app/store";
+
+interface UserProfile {
+  id: string;
+  email: string;
+  username: string;
+  lastName: string;
+  firstName: string;
+  avatar: string;
+}
 
 interface UserState {
-  id: string | null;
-  profile: {
-    email: string;
-    username: string;
-    lastName: string;
-    firstName: string;
-    avatar: string;
-  } | null;
+  profile: UserProfile | null;
 }
 
 const initialState: UserState = {
-  id: null,
   profile: null,
 };
+
+const userSlice = createSlice({
+  name: "user",
+  initialState,
+  reducers: {
+    signin: (state, action: PayloadAction<UserProfile>) => {
+      state.profile = action.payload;
+    },
+    signout: (state) => {
+      state.profile = null;
+    },
+  },
+});
+
+export const { signin, signout } = userSlice.actions;
+
+export const selIsSignedIn = (state: RootState) => !!state.user.profile?.id;
+
+export const userReducer = userSlice.reducer;
