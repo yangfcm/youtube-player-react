79d825c2658544abfb053e4a566d3b730e616383
Fan Yang <yangfcm16@gmail.com>
2023-01-21 00:30:52 +1030

    Make search bar work.

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,11 +1,13 @@
 import { Routes, Route } from "react-router-dom";
 import { Home } from "./pages/Home";
 import { Subscriptions } from "./pages/Subscriptions";
+import { SearchResults } from "./pages/SearchResults";
 
 export function Router() {
   return (
     <Routes>
       <Route path="/subscriptions" element={<Subscriptions />} />
+      <Route path="/search" element={<SearchResults />} />
       <Route path="/" element={<Home />} />
     </Routes>
   );
--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -1,11 +1,28 @@
+import { useState, useCallback, FormEvent } from "react";
+import { useNavigate } from "react-router-dom";
 import Paper from "@mui/material/Paper";
 import InputBase from "@mui/material/InputBase";
 import IconButton from "@mui/material/IconButton";
 import SearchIcon from "@mui/icons-material/Search";
 
 export function SearchBar() {
+  const navigate = useNavigate();
+  const [query, setQuery] = useState("");
+  const handleSearch = useCallback(
+    (e: FormEvent<HTMLFormElement>) => {
+      e.preventDefault();
+      if (!query.trim()) return;
+      navigate(`/search?q=${query}`);
+    },
+    [query, navigate]
+  );
+
   return (
-    <Paper component="form" sx={{ mr: { xs: 0, sm: 1 } }}>
+    <Paper
+      component="form"
+      sx={{ mr: { xs: 0, sm: 1 } }}
+      onSubmit={handleSearch}
+    >
       <InputBase
         placeholder="Search"
         sx={{
@@ -16,8 +33,11 @@ export function SearchBar() {
             xl: "300px",
           },
         }}
+        onChange={(e) => {
+          setQuery(e.target.value);
+        }}
       />
-      <IconButton type="button">
+      <IconButton type="submit">
         <SearchIcon />
       </IconButton>
     </Paper>
