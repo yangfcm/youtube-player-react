444a7e4ae04d9ee670c60faa8dfdf7fa41f60d86
Fan Yang <yangfcm16@gmail.com>
2023-01-21 22:08:15 +1030

    Fix search reducer to consider when search query changed.

--- src/pages/SearchResults.tsx
+++ src/pages/SearchResults.tsx
@@ -1,8 +1,48 @@
+import Box from "@mui/material/Box";
+import { useLocation } from "react-router-dom";
+import { LoadingSpinner } from "../components/LoadingSpinner";
 import { useSearchResults } from "../features/search/useSearchResults";
+import { MoreButton } from "../components/MoreButton";
+import { ErrorMessage } from "../components/ErrorMessage";
+import { AsyncStatus } from "../settings/types";
 
 export function SearchResults() {
-  const { searchResults, status, error, fetchMore, hasMore } =
-    useSearchResults("shitou");
-  console.log(searchResults, status, error);
-  return <>Search results</>;
+  const location = useLocation();
+  const query = location.search.substring(1);
+  const vars = query.split("&");
+  let q = "";
+  for (let i = 0; i < vars.length; i++) {
+    const pair = vars[i].split("=");
+    if (pair[0] === "q") {
+      q = decodeURIComponent(pair[1]);
+      break;
+    }
+  }
+  const { searchResults, status, error, fetchMore, hasMore, queryChanged } =
+    useSearchResults(q);
+
+  if (
+    (!searchResults?.length || queryChanged) &&
+    status === AsyncStatus.LOADING
+  ) {
+    return <LoadingSpinner />;
+  }
+
+  return (
+    <Box sx={{ pb: 2 }}>
+      <ErrorMessage open={status === AsyncStatus.FAIL}>{error}</ErrorMessage>
+      {searchResults &&
+        searchResults.map((result) => {
+          return <div>{result.snippet.title}</div>;
+        })}
+      {hasMore && (
+        <MoreButton
+          loading={status === AsyncStatus.LOADING}
+          onClick={fetchMore}
+        >
+          More
+        </MoreButton>
+      )}
+    </Box>
+  );
 }
