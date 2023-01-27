9f968c8c9e2923779600f0ddf1efaf940ae27603
Fan Yang <yangfcm16@gmail.com>
2023-01-27 11:28:47 +1030

    Remove card action area.

--- src/components/VideoCard.tsx
+++ src/components/VideoCard.tsx
@@ -3,7 +3,6 @@ import Box from "@mui/material/Box";
 import MuiLink from "@mui/material/Link";
 import Card from "@mui/material/Card";
 import CardContent from "@mui/material/CardContent";
-import CardActionArea from "@mui/material/CardActionArea";
 import Typography from "@mui/material/Typography";
 import { VideoSnippetStats } from "../features/video/types";
 import { fromNow, formatNumber } from "../app/utils";
@@ -20,52 +19,50 @@ export function VideoCard({ video }: { video: VideoSnippetStats }) {
           style={{ width: "100%", height: "auto" }}
         />
       </Link>
-      <CardActionArea>
-        <CardContent
+      <CardContent
+        sx={{
+          px: 1,
+          pt: "3px",
+        }}
+      >
+        <MuiLink
+          component={Link}
+          to={`/video/${video.id as string}`}
+          underline="none"
+          variant="subtitle1"
+          title={video.snippet.title}
           sx={{
-            px: 1,
-            pt: "3px",
+            display: "block",
+            lineHeight: "23px",
+            height: {
+              xs: "auto",
+              sm: "46px",
+            },
+            overflow: "hidden",
+            textOverflow: "ellipsis",
+            mb: 1,
           }}
         >
+          {video.snippet.title}
+        </MuiLink>
+        <Box sx={{ mb: "5px" }}>
           <MuiLink
             component={Link}
-            to={`/video/${video.id as string}`}
-            underline="none"
-            variant="subtitle1"
-            title={video.snippet.title}
-            sx={{
-              display: "block",
-              lineHeight: "23px",
-              height: {
-                xs: "auto",
-                sm: "46px",
-              },
-              overflow: "hidden",
-              textOverflow: "ellipsis",
-              mb: 1,
-            }}
+            to={`/channel/${video.snippet.channelId}`}
+            underline="hover"
+            variant="body2"
+            color="inherit"
           >
-            {video.snippet.title}
+            {video.snippet.channelTitle}
           </MuiLink>
-          <Box sx={{ mb: "5px" }}>
-            <MuiLink
-              component={Link}
-              to={`/channel/${video.snippet.channelId}`}
-              underline="hover"
-              variant="body2"
-              color="inherit"
-            >
-              {video.snippet.channelTitle}
-            </MuiLink>
-          </Box>
-          <Typography variant="caption">
-            <>
-              {formatNumber(parseInt(video.statistics.viewCount)) + " views"} •{" "}
-              {fromNow(video.snippet.publishedAt)}
-            </>
-          </Typography>
-        </CardContent>
-      </CardActionArea>
+        </Box>
+        <Typography variant="caption">
+          <>
+            {formatNumber(parseInt(video.statistics.viewCount)) + " views"} •{" "}
+            {fromNow(video.snippet.publishedAt)}
+          </>
+        </Typography>
+      </CardContent>
     </Card>
   );
 }
