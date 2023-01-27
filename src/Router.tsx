43970edadafa35c96730e8137f49e31edcd1de66
Fan Yang <yangfcm16@gmail.com>
2023-01-27 13:36:06 +1030

    Implement fetch video for video page.

--- src/Router.tsx
+++ src/Router.tsx
@@ -2,12 +2,14 @@ import { Routes, Route } from "react-router-dom";
 import { Home } from "./pages/Home";
 import { Subscriptions } from "./pages/Subscriptions";
 import { SearchResults } from "./pages/SearchResults";
+import { Video } from "./pages/Video";
 
 export function Router() {
   return (
     <Routes>
       <Route path="/subscriptions" element={<Subscriptions />} />
       <Route path="/search" element={<SearchResults />} />
+      <Route path="/video/:id" element={<Video />} />
       <Route path="/" element={<Home />} />
     </Routes>
   );
--- src/pages/Video.tsx
+++ src/pages/Video.tsx
@@ -1,3 +1,37 @@
+import { useParams } from "react-router-dom";
+import { useVideo } from "../features/video/useVideo";
+import Box from "@mui/material/Box";
+import Alert from "@mui/material/Alert";
+import { LoadingSpinner } from "../components/LoadingSpinner";
+import { AsyncStatus } from "../settings/types";
+import { ErrorMessage } from "../components/ErrorMessage";
+
+function NoVideo() {
+  return (
+    <Box sx={{ display: "d-flex", justifyContent: "center" }}>
+      <Alert
+        variant="filled"
+        severity="error"
+        sx={{ justifyContent: "center" }}
+      >
+        The video isn't available
+      </Alert>
+    </Box>
+  );
+}
+
 export function Video() {
-  return <>Video page</>;
+  const { id } = useParams();
+  const { video, status, error } = useVideo(id);
+  console.log(id);
+  if (status === AsyncStatus.LOADING) return <LoadingSpinner />;
+  if (!video && status === AsyncStatus.SUCCESS) {
+    return <NoVideo />;
+  }
+  return (
+    <Box>
+      <ErrorMessage open={status === AsyncStatus.FAIL}>{error}</ErrorMessage>
+      Video page
+    </Box>
+  );
 }
