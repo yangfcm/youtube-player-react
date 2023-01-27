32cece4dfbd27fa46c634874ca789ba2156efe02
Fan Yang <yangfcm16@gmail.com>
2023-01-27 22:59:50 +1030

    Add loader and error message to relevant videos list.

--- src/components/RelevantVideos.tsx
+++ src/components/RelevantVideos.tsx
@@ -1,11 +1,20 @@
 import Box from "@mui/material/Box";
+import Alert from "@mui/material/Alert";
 import { useRelevantVideos } from "../features/search/useRelevantVideos";
 import { VideoItem } from "./VideoItem";
+import { LoadingSpinner } from "../components/LoadingSpinner";
+import { AsyncStatus } from "../settings/types";
 
 export function RelevantVideos({ videoId }: { videoId: string }) {
-  const { videos } = useRelevantVideos(videoId);
+  const { videos, status, error } = useRelevantVideos(videoId);
+  if (status === AsyncStatus.LOADING) return <LoadingSpinner />;
   return (
     <>
+      {status === AsyncStatus.FAIL && (
+        <Alert severity="error" sx={{ justifyContent: "center" }}>
+          {error}
+        </Alert>
+      )}
       {videos &&
         videos.map((video, index) => (
           <Box sx={{ mb: 1 }}>
