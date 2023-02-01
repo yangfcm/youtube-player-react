7a42f413550b803489bd187aa5de2e92c59cd6af
Fan Yang <yangfcm16@gmail.com>
2023-02-01 16:33:22 +1030

    Add route for playlist page.

--- src/Router.tsx
+++ src/Router.tsx
@@ -3,12 +3,14 @@ import { Home } from "./pages/Home";
 import { Subscriptions } from "./pages/Subscriptions";
 import { SearchResults } from "./pages/SearchResults";
 import { Video } from "./pages/Video";
+import { PlayList } from "./pages/PlayList";
 
 export function Router() {
   return (
     <Routes>
       <Route path="/subscriptions" element={<Subscriptions />} />
       <Route path="/search" element={<SearchResults />} />
+      <Route path="/playlist" element={<PlayList />} />
       <Route path="/video/:id" element={<Video />} />
       <Route path="/" element={<Home />} />
     </Routes>
