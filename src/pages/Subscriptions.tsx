fa220dd68b83d9f7dfa52407f31a2204cb06c209
Fan Yang <yangfcm16@gmail.com>
2023-02-01 16:20:17 +1030

    Add channel card.

--- src/pages/Subscriptions.tsx
+++ src/pages/Subscriptions.tsx
@@ -1,12 +1,14 @@
 import Box from "@mui/material/Box";
 import Grid from "@mui/material/Grid";
 import Alert from "@mui/material/Alert";
+import Typography from "@mui/material/Typography";
 import { RequireAuth } from "../components/RequireAuth";
 import { useSubscriptions } from "../features/user/useSubscriptions";
 import { ErrorMessage } from "../components/ErrorMessage";
 import { LoadingSpinner } from "../components/LoadingSpinner";
 import { MoreButton } from "../components/MoreButton";
 import { AsyncStatus } from "../settings/types";
+import { ChannelCard } from "../components/ChannelCard";
 
 function NoSubscriptions() {
   return (
@@ -34,19 +36,28 @@ export function Subscriptions() {
   return (
     <RequireAuth>
       <ErrorMessage open={status === AsyncStatus.FAIL}>{error}</ErrorMessage>
+      <Typography variant="h5" sx={{ mb: 2 }}>
+        Subscriptions
+      </Typography>
       {status === AsyncStatus.IDLE ? (
         <></>
       ) : status === AsyncStatus.LOADING && subscriptions.length === 0 ? (
         <LoadingSpinner />
-      ) : subscriptions.length === 0 ? (
+      ) : status === AsyncStatus.SUCCESS && subscriptions.length === 0 ? (
         <NoSubscriptions />
       ) : (
         <Box>
-          <Grid container spacing={2}>
+          <Grid container spacing={2} sx={{ pb: 2 }}>
             {subscriptions.map((subscription) => {
               return (
-                <Grid item xs={12} sm={6} md={4} lg={3} key={subscription.id}>
-                  {subscription.snippet.title}
+                <Grid item xs={6} sm={3} lg={2} key={subscription.id}>
+                  <ChannelCard
+                    id={subscription.id}
+                    title={subscription.snippet.title}
+                    imageUrl={
+                      subscription.snippet.thumbnails.default?.url || ""
+                    }
+                  />
                 </Grid>
               );
             })}
