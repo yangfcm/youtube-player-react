eaf06d9cb3514f4c3cc83bece866921b42d0848f
Fan Yang <yangfcm16@gmail.com>
2023-02-01 16:33:22 +1030

    Add route for playlist page.

--- src/components/SidebarMenu.tsx
+++ src/components/SidebarMenu.tsx
@@ -6,6 +6,7 @@ import ListItemIcon from "@mui/material/ListItemIcon";
 import ListItemText from "@mui/material/ListItemText";
 import MuiLink from "@mui/material/Link";
 import SubscriptionsIcon from "@mui/icons-material/Subscriptions";
+import ViewListIcon from "@mui/icons-material/ViewList";
 import HomeIcon from "@mui/icons-material/Home";
 
 export function SidebarMenu() {
@@ -28,6 +29,16 @@ export function SidebarMenu() {
               <SubscriptionsIcon />
             </ListItemIcon>
             <ListItemText primary="Subscriptions" />
+          </ListItemButton>
+        </ListItem>
+      </MuiLink>
+      <MuiLink component={Link} to="/playlist" underline="none">
+        <ListItem disablePadding>
+          <ListItemButton>
+            <ListItemIcon>
+              <ViewListIcon />
+            </ListItemIcon>
+            <ListItemText primary="Play List" />
           </ListItemButton>
         </ListItem>
       </MuiLink>
