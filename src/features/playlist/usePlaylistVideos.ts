707750f35fcd44c7474babbf58c65c0130068d9b
Fan Yang <yangfcm16@gmail.com>
2023-02-02 15:35:13 +1030

    Add hook for playlist videos.

---
+++ src/features/playlist/usePlaylistVideos.ts
@@ -0,0 +1,42 @@
+import { useCallback, useEffect } from "react";
+import { useSelector } from "react-redux";
+import { useAppDispatch } from "../../app/hooks";
+import { RootState } from "../../app/store";
+import { fetchPlaylistVideos } from "./playlistSlice";
+
+export function usePlaylistVideos(playlistId: string) {
+  const dispatch = useAppDispatch();
+  const playlistVideos = useSelector(
+    (state: RootState) => state.playlist.playlists[playlistId]?.items || []
+  );
+  const asyncStatus = useSelector((state: RootState) => state.playlist.status);
+  const error = useSelector((state: RootState) => state.playlist.error);
+  const nextPageToken = useSelector(
+    (state: RootState) => state.playlist.playlists[playlistId].nextPageToken
+  );
+
+  const fetchMore = useCallback(() => {
+    if (nextPageToken) {
+      dispatch(
+        fetchPlaylistVideos({
+          playlistId,
+          pageToken: nextPageToken,
+        })
+      );
+    }
+  }, [dispatch, nextPageToken, playlistId]);
+
+  useEffect(() => {
+    if (playlistId && playlistVideos.length === 0) {
+      dispatch(fetchPlaylistVideos({ playlistId }));
+    }
+  }, [playlistId, dispatch, playlistVideos]);
+
+  return {
+    playlistVideos,
+    status: asyncStatus,
+    error,
+    hasMore: !!nextPageToken,
+    fetchMore,
+  };
+}
