68d826e9cbcd0a26949150575443e08b12f8811a
Fan Yang <yangfcm16@gmail.com>
2023-02-03 01:17:38 +1030

    Implement reducer to fetch channel videos and playlists.

--- src/features/channel/channelAPI.ts
+++ src/features/channel/channelAPI.ts
@@ -1,6 +1,12 @@
 import { AxiosResponse } from "axios";
 import { appAxios } from "../../settings/api";
-import { PART_SNIPPET_STATS } from "../../settings/constant";
+import {
+  MAX_RESULTS_24,
+  PART_SNIPPET_CONTENT_STATUS,
+  PART_SNIPPET_STATS,
+} from "../../settings/constant";
+import { PlayListsResponse } from "../playlist/types";
+import { VideosResponse } from "../video/types";
 import { ChannelDetailsResponse } from "./types";
 
 export async function fetchChannelProfileAPI(
@@ -8,12 +14,38 @@ export async function fetchChannelProfileAPI(
 ): Promise<AxiosResponse<ChannelDetailsResponse>> {
   return await appAxios.get("/channels", {
     params: {
-      part: PART_SNIPPET_STATS,
       id: channelId,
+      part: PART_SNIPPET_STATS,
     },
   });
 }
 
-export async function fetchChannelVideosAPI() {}
+export async function fetchChannelVideosAPI(
+  channelId: string,
+  options: Record<string, string> = {}
+): Promise<AxiosResponse<VideosResponse>> {
+  return await appAxios.get("/search", {
+    params: {
+      channelId,
+      part: PART_SNIPPET_STATS,
+      order: "date",
+      type: "video",
+      maxResults: MAX_RESULTS_24,
+      ...options,
+    },
+  });
+}
 
-export async function fetchChannelPlaylistsAPI() {}
+export async function fetchChannelPlaylistsAPI(
+  channelId: string,
+  options: Record<string, string> = {}
+): Promise<AxiosResponse<PlayListsResponse>> {
+  return await appAxios.get("/playlists", {
+    params: {
+      channelId,
+      maxResults: MAX_RESULTS_24,
+      part: PART_SNIPPET_CONTENT_STATUS,
+      ...options,
+    },
+  });
+}
