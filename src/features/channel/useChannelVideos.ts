82be30bb52d9b9214da07813ec0eff0512d845df
Fan Yang <yangfcm16@gmail.com>
2023-02-03 01:37:40 +1030

    Add hook and fetch channel videos.

---
+++ src/features/channel/useChannelVideos.ts
@@ -0,0 +1,45 @@
+import { useCallback, useEffect } from "react";
+import { useSelector } from "react-redux";
+import { useAppDispatch } from "../../app/hooks";
+import { RootState } from "../../app/store";
+import { fetchChannelVideos } from "./channelSlice";
+
+export function useChannelVideos(channelId: string) {
+  const dispatch = useAppDispatch();
+  const asyncStatus = useSelector(
+    (state: RootState) => state.channel.videos.status
+  );
+  const error = useSelector((state: RootState) => state.channel.videos.error);
+
+  const channelVideos = useSelector(
+    (state: RootState) => state.channel.videos.data[channelId]?.items || []
+  );
+  const nextPageToken = useSelector(
+    (state: RootState) => state.channel.videos.data[channelId]?.nextPageToken
+  );
+
+  const fetchMore = useCallback(() => {
+    if (nextPageToken) {
+      dispatch(
+        fetchChannelVideos({
+          channelId,
+          pageToken: nextPageToken,
+        })
+      );
+    }
+  }, [dispatch, nextPageToken, channelId]);
+
+  useEffect(() => {
+    if (channelId && channelVideos.length === 0) {
+      dispatch(fetchChannelVideos({ channelId }));
+    }
+  }, [channelId, channelVideos.length, dispatch]);
+
+  return {
+    channelVideos,
+    status: asyncStatus,
+    error,
+    hasMore: !!nextPageToken,
+    fetchMore,
+  };
+}
