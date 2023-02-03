22431268b2cb8d7376d1132ec8ff47da8ffaaa3b
Fan Yang <yangfcm16@gmail.com>
2023-02-03 10:31:45 +1030

    Add default error message.

--- src/settings/constant.ts
+++ src/settings/constant.ts
@@ -4,5 +4,5 @@ export const MAX_RESULTS_15 = 15;
 export const PART_SNIPPET_STATS = "snippet,statistics";
 export const PART_SNIPPET = "snippet";
 export const PART_SNIPPET_CONTENT_STATUS = "snippet,contentDetails,status";
-
+export const DEFAULT_ERROR_MESSAGE = "Something bad happens";
 export const SIDEBAR_WIDTH = 220;
