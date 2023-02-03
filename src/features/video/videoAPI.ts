9f4c2fbcd823c5f50738b8d7c4b5efd61b14df67
Fan Yang <yangfcm16@gmail.com>
2023-02-03 11:41:19 +1030

    Update argument name.

--- src/features/video/videoAPI.ts
+++ src/features/video/videoAPI.ts
@@ -4,13 +4,13 @@ import { MAX_RESULTS_24, PART_SNIPPET_STATS } from "../../settings/constant";
 import { VideoResponse, VideosResponse } from "./types";
 
 export async function fetchVideosAPI(
-  filter: Record<string, string> = {}
+  options: Record<string, string> = {}
 ): Promise<AxiosResponse<VideosResponse>> {
   return await appAxios.get("/videos", {
     params: {
       part: PART_SNIPPET_STATS,
       maxResults: MAX_RESULTS_24,
-      ...filter,
+      ...options,
     },
   });
 }
