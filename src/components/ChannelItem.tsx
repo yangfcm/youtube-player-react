c2c08aa56d76149ea3bea66bd85cad3f78be9158
Fan Yang <yangfcm16@gmail.com>
2023-02-09 13:30:38 +1030

    Tweak channel item's height.

--- src/components/ChannelItem.tsx
+++ src/components/ChannelItem.tsx
@@ -24,7 +24,7 @@ export function ChannelItem({ channel }: { channel: ChannelPropsType }) {
       sx={{
         display: "flex",
         height: {
-          sm: 160,
+          sm: 165,
           xs: "auto",
         },
         flexDirection: {
