7e8523d5c2540580ac4ff46e6890a47f1d3d0419
Fan Yang <yangfcm16@gmail.com>
2023-02-09 13:43:04 +1030

    Show different text to respond to subscription status.

--- src/components/SubscribeButton.tsx
+++ src/components/SubscribeButton.tsx
@@ -3,20 +3,29 @@ import { RequireAuth } from "./RequireAuth";
 import { useSubscribe } from "../features/user/useSubscribe";
 import { AsyncStatus } from "../settings/types";
 import { UNSUBSCRIBED } from "../settings/constant";
+import { useState } from "react";
 
 function SubscribeButtonComp({ channelId }: { channelId: string }) {
   const { status, subscriptionId } = useSubscribe(channelId);
+  const subscribed = subscriptionId !== UNSUBSCRIBED;
+  const [subscribedText, setSubscribedText] = useState("Subscribed");
 
   if (!subscriptionId) return null;
 
   return (
     <Button
-      variant="outlined"
+      variant={subscribed ? "contained" : "outlined"}
       size="small"
       disabled={status === AsyncStatus.LOADING}
       sx={{ width: "130px" }}
+      onMouseOver={() => {
+        if (subscribed) setSubscribedText("Unsubscribe");
+      }}
+      onMouseLeave={() => {
+        if (subscribed) setSubscribedText("Subscribed");
+      }}
     >
-      {subscriptionId === UNSUBSCRIBED ? "Unsubscribed" : "Subscribed"}
+      {subscribed ? subscribedText : "Subscribe"}
     </Button>
   );
 }
