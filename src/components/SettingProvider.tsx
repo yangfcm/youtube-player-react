e2030a6a62869fd26d936d4c89bd55a9d5579901
Fan Yang <yangfcm16@gmail.com>
2023-02-11 00:58:13 +1030

    Remove useRegions.

--- src/components/SettingProvider.tsx
+++ src/components/SettingProvider.tsx
@@ -1,8 +1,6 @@
 import { useCategories } from "../features/setting/useCategories";
-import { useRegions } from "../features/setting/useRegions";
 
 export function SettingProvider({ children }: { children: React.ReactNode }) {
   useCategories();
-  // useRegions();
   return <>{children}</>;
 }
