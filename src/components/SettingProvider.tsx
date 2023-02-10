fa0faf6318cec4bc14d6be976f8e9da74918224c
Fan Yang <yangfcm16@gmail.com>
2023-02-10 17:56:16 +1030

    Remove redundant component.

--- src/components/SettingProvider.tsx
+++ src/components/SettingProvider.tsx
@@ -2,7 +2,6 @@ import { useCategories } from "../features/setting/useCategories";
 import { useRegions } from "../features/setting/useRegions";
 
 export function SettingProvider({ children }: { children: React.ReactNode }) {
-  console.log("setting provider!");
   useCategories();
   // useRegions();
   return <>{children}</>;
