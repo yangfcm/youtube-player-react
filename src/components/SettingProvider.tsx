139ece93f3101ae5d19181e635d93f5b1eb22208
Fan Yang <yangfcm16@gmail.com>
2023-02-11 17:29:27 +1030

    Fetch region in SettingProvider.

--- src/components/SettingProvider.tsx
+++ src/components/SettingProvider.tsx
@@ -1,6 +1,8 @@
 import { useCategories } from "../features/setting/useCategories";
+import { useRegion } from "../features/setting/useRegion";
 
 export function SettingProvider({ children }: { children: React.ReactNode }) {
+  useRegion();
   useCategories();
   return <>{children}</>;
 }
