091dc69501649981f4816e3a83a6f8cb6dfb434f
Fan Yang <yangfcm16@gmail.com>
2023-02-11 17:23:08 +1030

    Make categories dependent on location.

--- src/components/SettingProvider.tsx
+++ src/components/SettingProvider.tsx
@@ -1,8 +1,6 @@
 import { useCategories } from "../features/setting/useCategories";
-import { useLocation } from "../features/setting/useLocation";
 
 export function SettingProvider({ children }: { children: React.ReactNode }) {
   useCategories();
-  useLocation();
   return <>{children}</>;
 }
