131c4233000f4715204dfe1d7e3a12b0ae18c30f
Fan Yang <yangfcm16@gmail.com>
2023-02-17 14:37:56 +1030

    Add error handling.

--- src/components/GoogleLogin.tsx
+++ src/components/GoogleLogin.tsx
@@ -2,7 +2,7 @@ import { GoogleLoginBase, GoogleLoginResponse } from "./GoogleLoginBase";
 import { useAuth } from "../features/user/useAuth";
 
 export function GoogleLogin() {
-  const { signin } = useAuth();
+  const { signin, signout } = useAuth();
 
   const handleSuccessSignin = (response?: GoogleLoginResponse) => {
     const auth = (response as GoogleLoginResponse).getAuthResponse();
@@ -22,20 +22,17 @@ export function GoogleLogin() {
     localStorage.setItem("token", "Bearer " + auth.access_token);
   };
 
-  // const handleFailureSignin = () => {
-  //   signout();
-  //   localStorage.removeItem("token");
-  // };
+  const handleFailureSignin = () => {
+    signout();
+    localStorage.removeItem("token");
+  };
 
   return (
     <GoogleLoginBase
       isLoggedIn={false}
       buttonText="Log in"
       onSuccess={handleSuccessSignin}
-      // onFailure={handleFailureSignin}
-      // cookiePolicy={"single_host_origin"}
-      // isSignedIn={true}
-      // className="auth__google-login-button"
+      onFailure={handleFailureSignin}
     />
   );
 }
