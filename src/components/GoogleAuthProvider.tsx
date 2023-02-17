7ce4e3a53a9b2b6cf4ce2df82b72d3645f156b44
Fan Yang <yangfcm16@gmail.com>
2023-02-17 14:24:50 +1030

    Write own google auth button.

--- src/components/GoogleAuthProvider.tsx
+++ src/components/GoogleAuthProvider.tsx
@@ -3,6 +3,7 @@ import { gapi } from "gapi-script";
 import { GapiLoadError } from "../settings/types";
 import { ErrorMessage } from "./ErrorMessage";
 import { LoadingSpinner } from "./LoadingSpinner";
+import { useAuth } from "../features/user/useAuth";
 
 export function GoogleAuthProvider({
   children,
@@ -11,24 +12,52 @@ export function GoogleAuthProvider({
 }) {
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState("");
+  const { signin, signout } = useAuth();
 
   useEffect(() => {
-    const initClient = () => {
-      gapi.client
-        .init({
-          clientId: process.env.REACT_APP_CLIENT_ID,
-          scope: "openid ",
-        })
-        .then(() => {
-          setLoading(false);
-        })
-        .catch((e: GapiLoadError) => {
-          setLoading(false);
-          setError(e.details);
-        });
+    const initClient = async () => {
+      const googleAuth = await gapi?.auth2.getAuthInstance();
+      if (!googleAuth) {
+        gapi.client
+          .init({
+            clientId: process.env.REACT_APP_CLIENT_ID,
+            scope: "openid ",
+          })
+          .then(() => {
+            setLoading(false);
+          })
+          .catch((e: GapiLoadError) => {
+            setLoading(false);
+            setError(e.details);
+            signout();
+            return;
+          });
+      }
+      const isSignedIn = googleAuth.isSignedIn.get();
+      if (isSignedIn) {
+        const currentUser = googleAuth.currentUser.get();
+        const authResponse = currentUser.getAuthResponse();
+        const userProfile = currentUser.getBasicProfile();
+        signin(
+          {
+            id: userProfile.getId(),
+            firstName: userProfile.getGivenName(),
+            lastName: userProfile.getFamilyName(),
+            email: userProfile.getEmail(),
+            avatar: userProfile.getImageUrl(),
+            username: userProfile.getName(),
+          },
+          authResponse.access_token,
+          authResponse.expires_at
+        );
+      } else {
+        signout();
+      }
+      setLoading(false);
+      setError("");
     };
     gapi.load("client:auth2", initClient);
-  }, []);
+  }, [signin, signout]);
 
   if (loading) return <LoadingSpinner />;
 
---
+++ src/components/GoogleLoginBase.tsx
@@ -0,0 +1,244 @@
+import { useState } from "react";
+import { gapi } from "gapi-script";
+
+function GoogleIcon({ active = true }: { active?: boolean }) {
+  return (
+    <div
+      style={{
+        background: active ? "#eee" : "#fff",
+        padding: "6px",
+        borderRadius: 2,
+        display: "flex",
+        alignItems: "center",
+      }}
+    >
+      <svg width="18" height="18" xmlns="http://www.w3.org/2000/svg">
+        <g fill="#000" fillRule="evenodd">
+          <path
+            d="M9 3.48c1.69 0 2.83.73 3.48 1.34l2.54-2.48C13.46.89 11.43 0 9 0 5.48 0 2.44 2.02.96 4.96l2.91 2.26C4.6 5.05 6.62 3.48 9 3.48z"
+            fill="#EA4335"
+          />
+          <path
+            d="M17.64 9.2c0-.74-.06-1.28-.19-1.84H9v3.34h4.96c-.1.83-.64 2.08-1.84 2.92l2.84 2.2c1.7-1.57 2.68-3.88 2.68-6.62z"
+            fill="#4285F4"
+          />
+          <path
+            d="M3.88 10.78A5.54 5.54 0 0 1 3.58 9c0-.62.11-1.22.29-1.78L.96 4.96A9.008 9.008 0 0 0 0 9c0 1.45.35 2.82.96 4.04l2.92-2.26z"
+            fill="#FBBC05"
+          />
+          <path
+            d="M9 18c2.43 0 4.47-.8 5.96-2.18l-2.84-2.2c-.76.53-1.78.9-3.12.9-2.38 0-4.4-1.57-5.12-3.74L.97 13.04C2.45 15.98 5.48 18 9 18z"
+            fill="#34A853"
+          />
+          <path fill="none" d="M0 0h18v18H0z" />
+        </g>
+      </svg>
+    </div>
+  );
+}
+
+interface AuthResponse {
+  readonly access_token: string;
+  readonly id_token: string;
+  readonly login_hint: string;
+  readonly scope: string;
+  readonly expires_in: number;
+  readonly first_issued_at: number;
+  readonly expires_at: number;
+}
+
+interface BasicProfile {
+  getId(): string;
+  getEmail(): string;
+  getName(): string;
+  getGivenName(): string;
+  getFamilyName(): string;
+  getImageUrl(): string;
+}
+
+interface SignInOptions {
+  readonly scope?: string;
+  readonly app_package_name?: string;
+  readonly fetch_basic_profile?: boolean;
+  readonly prompt?: string;
+}
+
+interface GrantOfflineAccessOptions {
+  readonly scope?: string;
+  readonly redirect_uri?: string;
+}
+
+export interface GoogleLoginResponseOffline {
+  readonly code: string;
+}
+
+// Based on https://developers.google.com/identity/sign-in/web/reference
+export interface GoogleLoginResponse {
+  getBasicProfile(): BasicProfile;
+  getAuthResponse(includeAuthorizationData?: boolean): AuthResponse;
+  reloadAuthResponse(): Promise<AuthResponse>;
+  getGrantedScopes(): string;
+  getHostedDomain(): string;
+  getId(): string;
+  isSignedIn(): boolean;
+  hasGrantedScopes(scopes: string): boolean;
+  disconnect(): void;
+  grantOfflineAccess(
+    options: GrantOfflineAccessOptions
+  ): Promise<GoogleLoginResponseOffline>;
+  signIn(options: SignInOptions): Promise<any>;
+  grant(options: SignInOptions): Promise<any>;
+  // google-login.js sez: offer renamed response keys to names that match use
+  googleId: string;
+  tokenObj: AuthResponse;
+  tokenId: string;
+  accessToken: string;
+  readonly code?: string; //does not exist but here to satisfy typescript compatibility
+  profileObj: {
+    googleId: string;
+    imageUrl: string;
+    email: string;
+    name: string;
+    givenName: string;
+    familyName: string;
+  };
+}
+
+type GoogleLoginPropsType = {
+  onSuccess: (res?: GoogleLoginResponse) => void;
+  onFailure?: () => void;
+  isLoggedIn: boolean;
+  active?: boolean;
+  children?: React.ReactNode;
+  buttonText?: string;
+  theme?: "dark" | "light";
+  disabled?: boolean;
+  disabledStyle?: Record<string, any>;
+  isSignedIn?: boolean;
+};
+export function GoogleLoginBase(props: GoogleLoginPropsType) {
+  const {
+    isLoggedIn,
+    theme = "light",
+    disabled = false,
+    disabledStyle = { opacity: 0.6 },
+    buttonText,
+    children,
+    onSuccess,
+  } = props;
+
+  const [hovered, setHovered] = useState(false);
+  const [active, setActive] = useState(false);
+
+  const initialStyle = {
+    backgroundColor: theme === "dark" ? "rgb(66, 133, 244)" : "#fff",
+    display: "inline-flex",
+    alignItems: "center",
+    color: theme === "dark" ? "#fff" : "rgba(0, 0, 0, .54)",
+    boxShadow: "0 2px 2px 0 rgba(0, 0, 0, .24), 0 0 1px 0 rgba(0, 0, 0, .24)",
+    padding: 0,
+    borderRadius: 2,
+    border: "1px solid transparent",
+    fontSize: 14,
+    fontWeight: "500",
+    fontFamily: "Roboto, sans-serif",
+  };
+
+  const hoveredStyle = {
+    cursor: "pointer",
+    opacity: 0.9,
+  };
+
+  const activeStyle = {
+    cursor: "pointer",
+    backgroundColor: theme === "dark" ? "#3367D6" : "#eee",
+    color: theme === "dark" ? "#fff" : "rgba(0, 0, 0, .54)",
+    opacity: 1,
+  };
+
+  const defaultStyle = (() => {
+    if (disabled) {
+      return Object.assign({}, initialStyle, disabledStyle);
+    }
+
+    if (active) {
+      if (theme === "dark") {
+        return Object.assign({}, initialStyle, activeStyle);
+      }
+
+      return Object.assign({}, initialStyle, activeStyle);
+    }
+
+    if (hovered) {
+      return Object.assign({}, initialStyle, hoveredStyle);
+    }
+
+    return initialStyle;
+  })();
+
+  const login = async () => {
+    const googleAuth = gapi?.auth2.getAuthInstance();
+    if (!googleAuth) return;
+    try {
+      const res = await googleAuth.signIn({ prompt: "" });
+      const basicProfile = res.getBasicProfile();
+      const authResponse = res.getAuthResponse(true);
+      res.googleId = basicProfile.getId();
+      res.tokenObj = authResponse;
+      res.tokenId = authResponse.id_token;
+      res.accessToken = authResponse.access_token;
+      res.profileObj = {
+        googleId: basicProfile.getId(),
+        imageUrl: basicProfile.getImageUrl(),
+        email: basicProfile.getEmail(),
+        name: basicProfile.getName(),
+        givenName: basicProfile.getGivenName(),
+        familyName: basicProfile.getFamilyName(),
+      };
+      onSuccess(res);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  const logout = async () => {
+    const googleAuth = gapi?.auth2.getAuthInstance();
+    try {
+      await googleAuth.signOut();
+      googleAuth.disconnect();
+      onSuccess();
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  const handleClickButton = (e: React.SyntheticEvent) => {
+    e.preventDefault();
+    if (isLoggedIn) logout();
+    else login();
+  };
+
+  return (
+    <div
+      style={defaultStyle}
+      onMouseEnter={() => setHovered(true)}
+      onMouseLeave={() => {
+        setHovered(false);
+        setActive(false);
+      }}
+      onMouseDown={() => setActive(true)}
+      onMouseUp={() => setActive(false)}
+    >
+      <GoogleIcon active={active} />
+      <span
+        style={{
+          padding: 10,
+          fontWeight: 500,
+        }}
+        onClick={handleClickButton}
+      >
+        {children || buttonText}
+      </span>
+    </div>
+  );
+}
