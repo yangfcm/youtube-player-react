ffe4459d870228e3404336ee6640c639f7446590
Fan Yang <yangfcm16@gmail.com>
2023-02-17 14:52:35 +1030

    Add not found page and return home button.

--- src/components/NoContent.tsx
+++ src/components/NoContent.tsx
@@ -1,20 +1,30 @@
+import { Link } from "react-router-dom";
 import Box from "@mui/material/Box";
+import MuiLink from "@mui/material/Link";
 import SentimentVeryDissatisfiedIcon from "@mui/icons-material/SentimentVeryDissatisfied";
 
 export function NoContent({ children }: { children: React.ReactNode }) {
   return (
-    <Box
-      sx={{
-        display: "flex",
-        AlignItems: "center",
-        justifyContent: "center",
-        py: 5,
-        PX: 2,
-      }}
-    >
-      <SentimentVeryDissatisfiedIcon />
-      &nbsp;
-      {children}
-    </Box>
+    <>
+      <Box
+        sx={{
+          display: "flex",
+          AlignItems: "center",
+          justifyContent: "center",
+          mt: 4,
+          mb: 1,
+          px: 2,
+        }}
+      >
+        <SentimentVeryDissatisfiedIcon />
+        &nbsp;
+        {children}
+      </Box>
+      <Box sx={{ display: "flex", justifyContent: "center" }}>
+        <MuiLink component={Link} to="/">
+          Back to Home
+        </MuiLink>
+      </Box>
+    </>
   );
 }
