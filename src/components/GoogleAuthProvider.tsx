2e8429ab1047cf71e11aa7fea92803a9155c4aa9
Fan Yang <yangfcm16@gmail.com>
2023-02-17 16:46:49 +1030

    Do not lose auth info.

--- src/components/GoogleAuthProvider.tsx
+++ src/components/GoogleAuthProvider.tsx
@@ -16,23 +16,21 @@ export function GoogleAuthProvider({
 
   useEffect(() => {
     const initClient = async () => {
+      gapi.client
+        .init({
+          clientId: process.env.REACT_APP_CLIENT_ID,
+          scope: "openid ",
+        })
+        .then(() => {
+          setLoading(false);
+        })
+        .catch((e: GapiLoadError) => {
+          setLoading(false);
+          setError(e.details);
+          signout();
+          return;
+        });
       const googleAuth = await gapi?.auth2.getAuthInstance();
-      if (!googleAuth) {
-        gapi.client
-          .init({
-            clientId: process.env.REACT_APP_CLIENT_ID,
-            scope: "openid ",
-          })
-          .then(() => {
-            setLoading(false);
-          })
-          .catch((e: GapiLoadError) => {
-            setLoading(false);
-            setError(e.details);
-            signout();
-            return;
-          });
-      }
       const isSignedIn = googleAuth.isSignedIn.get();
       if (isSignedIn) {
         const currentUser = googleAuth.currentUser.get();
