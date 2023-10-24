52f81e39c36acf7c7704d7c41349b46bb7fc3db0
yangfcm <yangfcm@outlook.com>
2023-10-24 22:27:15 +1030

    Add service to update user subscriptions in firestore db.

--- src/app/firebaseServices.ts
+++ src/app/firebaseServices.ts
@@ -1,12 +1,25 @@
-import { doc, getDoc } from "firebase/firestore";
+import { doc, getDoc, updateDoc } from "firebase/firestore";
 import { db } from "../settings/firebaseConfig";
 
 export const updaterUserSubscriptions = async (
   userId: string,
-  channelIds: string[],
+  channelId: string,
   action: "subscribe" | "unsubscribe"
 ) => {
   const userRef = doc(db, "users", userId);
   const userSnap = await getDoc(userRef);
-  console.log(userSnap);
+  const userData = userSnap.data();
+  if (!userData) return;
+
+  const currentSubscriptions: string[] = userData.subscriptions;
+  let updatedSubscriptions = [...currentSubscriptions];
+  if (action === "subscribe") {
+    updatedSubscriptions.push(channelId);
+  }
+  if (action === "unsubscribe") {
+    updatedSubscriptions = updatedSubscriptions.filter((c) => c !== channelId);
+  }
+  await updateDoc(userRef, {
+    subscriptions: updatedSubscriptions,
+  });
 };
