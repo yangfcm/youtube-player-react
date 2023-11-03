e0a18a77ebf6b3d1b7aec6097a7287e91c768fca
yangfcm <yangfcm@outlook.com>
2023-11-03 10:56:36 +1030

    Fix the renderer warning.

--- src/features/video/useDownloadVideo.ts
+++ src/features/video/useDownloadVideo.ts
@@ -1,18 +1,17 @@
 import { useCallback } from "react";
-import { useSelector } from "react-redux";
+import { shallowEqual, useSelector } from "react-redux";
 import { useAppDispatch } from "../../app/hooks";
 import { downloadVideo as downloadVideoAction } from "./videoSlice";
 import { DownloadParameter } from "./types";
 import { RootState } from "../../app/store";
+import { getVideoDownloadState } from "./selectors";
 
 export function useDownloadVideo({videoId, title, userId, filter}: DownloadParameter) {
   const dispatch = useAppDispatch();
-  const { url, expiredAt, status, error } = useSelector((state: RootState) => {
-    const video = state.video.video.item[videoId];
-    const key = filter === 'audioonly' ? 'downloadAudioonly' : 'downloadVideo';
-    return video?.[key] || {};
-  });
-
+  
+  const downloadState = useSelector((state: RootState) => getVideoDownloadState(state, { videoId, filter }), shallowEqual);
+  const { url, expiredAt, status, error } = downloadState;
+ 
   const downloadVideo = useCallback(() => {
     dispatch(downloadVideoAction({
       videoId, title, userId, filter,
