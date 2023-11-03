75d2b122de478fe6544a17f6ea11eac350365479
yangfcm <yangfcm@outlook.com>
2023-11-03 22:47:24 +1030

    Can repeatedly call a function in usePoll.

--- src/features/video/useDownloadVideo.ts
+++ src/features/video/useDownloadVideo.ts
@@ -1,12 +1,13 @@
 import { useCallback, useEffect } from "react";
 import axios from "axios";
 import { shallowEqual, useSelector } from "react-redux";
-import { useAppDispatch } from "../../app/hooks";
+import { useAppDispatch, usePoll } from "../../app/hooks";
 import { downloadVideo as downloadVideoAction } from "./videoSlice";
 import { DownloadParameter } from "./types";
 import { RootState } from "../../app/store";
 import { getVideoDownloadState } from "./selectors";
 import { DOWNLOAD_CANCELD_ERROR } from "../../settings/constant";
+import { AsyncStatus } from "../../settings/types";
 
 export function useDownloadVideo({
   videoId,
@@ -21,6 +22,7 @@ export function useDownloadVideo({
     return () => {
       cancelTokenSource.cancel(DOWNLOAD_CANCELD_ERROR);
     };
+    // eslint-disable-next-line
   }, []);
 
   const downloadState = useSelector(
@@ -28,6 +30,16 @@ export function useDownloadVideo({
     shallowEqual
   );
   const { url, expiredAt, status, error } = downloadState;
+
+  usePoll(
+    () => {
+      if (status === AsyncStatus.LOADING) {
+        console.log("called!");
+      }
+    },
+    { interval: 1000 },
+    status
+  );
 
   const downloadVideo = useCallback(() => {
     dispatch(
