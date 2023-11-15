64775e14cfcbd5c125f0573f97f0ef9cfd1dd362
Fan Yang <yangfcm16@gmail.com>
2023-11-15 16:19:51 +1030

    Specify return types for fetch user by token.

--- src/features/user/types.ts
+++ src/features/user/types.ts
@@ -34,6 +34,17 @@ export interface SubscriptionsResponse {
   nextPageToken?: string;
 }
 
+export interface UserInfoResponse {
+  email: string;
+  email_verified: boolean;
+  family_name: string;
+  given_name: string;
+  locale: string;
+  name: string;
+  picture: string;
+  sub: string;
+}
+
 export interface UserProfile {
   id: string;
   email: string;
