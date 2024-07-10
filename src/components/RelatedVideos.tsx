84dc346240865f06981236264756c6f25515bd88
Fan <yangfcm@outlook.com>
2024-07-10 22:54:21 +0930

    Add accordion to related videos section.

--- src/components/RelatedVideos.tsx
+++ src/components/RelatedVideos.tsx
@@ -1,4 +1,8 @@
 import Grid from "@mui/material/Grid";
+import Accordion from "@mui/material/Accordion";
+import AccordionSummary from "@mui/material/AccordionSummary";
+import AccordionDetails from "@mui/material/AccordionDetails";
+import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
 import Typography from "@mui/material/Typography";
 import { VideoCard } from "./VideoCard";
 import { RelatedVideo } from "../features/video/types";
@@ -15,29 +19,44 @@ export function RelatedVideos({
   }
 
   return (
-    <>
-      <Typography variant="h5" sx={{ mb: 2 }}>
-        Related Videos
-      </Typography>
-      <Grid container spacing={2}>
-        {videos.map((video) => {
-          return (
-            <Grid item xs={12} sm={6} md={4} lg={3} xl={2} key={video.videoId}>
-              <VideoCard
-                video={{
-                  id: video.videoId,
-                  title: video.title,
-                  imageUrl: video.thumbnail,
-                  channelId: video.channelId,
-                  channelTitle: video.channelTitle,
-                  viewCount: video.viewCount,
-                  publishedAt: video.publishedAt,
-                }}
-              />
-            </Grid>
-          );
-        })}
-      </Grid>
-    </>
+    <Accordion>
+      <AccordionSummary
+        id="related-videos-header"
+        expandIcon={<ExpandMoreIcon />}
+      >
+        <Typography variant="h5" sx={{ mb: 2 }}>
+          Related Videos
+        </Typography>
+      </AccordionSummary>
+      <AccordionDetails>
+        <Grid container spacing={2}>
+          {videos.map((video) => {
+            return (
+              <Grid
+                item
+                xs={12}
+                sm={6}
+                md={4}
+                lg={3}
+                xl={2}
+                key={video.videoId}
+              >
+                <VideoCard
+                  video={{
+                    id: video.videoId,
+                    title: video.title,
+                    imageUrl: video.thumbnail,
+                    channelId: video.channelId,
+                    channelTitle: video.channelTitle,
+                    viewCount: video.viewCount,
+                    publishedAt: video.publishedAt,
+                  }}
+                />
+              </Grid>
+            );
+          })}
+        </Grid>
+      </AccordionDetails>
+    </Accordion>
   );
 }
