90565579e233e03cb13838da9e940fdf83bdd6c7
Yang Fan <yangfcm16@gmail.com>
2024-07-10 23:09:26 +0930

    UI and use external download. (#76)
    * Merge with master
    
    * Improve video description.
    
    * Fix the dependency.
    
    * Update the max results fetched in timeline.
    
    * Do not show related videos section if there's no data.
    
    * Add playlist id to videos from playlist.
    
    * Make comments section collapsable.
    
    * Update max results.
    
    * Add collapsible playlist videos.
    
    * Add privacy status to playlist.
    
    * Remove the scroll bar.
    
    * Add accordion to related videos section.
    
    * Add icon to accordion header.
    
    ---------
    
    Co-authored-by: Fan Yang <fan@attackforge.com>

--- src/components/RelatedVideos.tsx
+++ src/components/RelatedVideos.tsx
@@ -1,7 +1,12 @@
 import Grid from "@mui/material/Grid";
+import Accordion from "@mui/material/Accordion";
+import AccordionSummary from "@mui/material/AccordionSummary";
+import AccordionDetails from "@mui/material/AccordionDetails";
+import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
 import Typography from "@mui/material/Typography";
 import { VideoCard } from "./VideoCard";
 import { RelatedVideo } from "../features/video/types";
+import VideoLibraryIcon from "@mui/icons-material/VideoLibrary";
 
 export function RelatedVideos({
   videos: videosProp,
@@ -15,29 +20,45 @@ export function RelatedVideos({
   }
 
   return (
-    <>
-      <Typography variant="h5" sx={{ mb: 2 }}>
-        Related Videos
-      </Typography>
-      <Grid container spacing={2}>
-        {videos.map((video) => {
-          return (
-            <Grid item xs={12} sm={6} md={4} lg={3} xl={2} key={video.videoId}>
-              <VideoCard
-                video={{
-                  id: video.videoId,
-                  title: video.title,
-                  imageUrl: video.thumbnail,
-                  channelId: video.channelId,
-                  channelTitle: video.channelTitle,
-                  viewCount: video.viewCount,
-                  publishedAt: video.publishedAt,
-                }}
-              />
-            </Grid>
-          );
-        })}
-      </Grid>
-    </>
+    <Accordion>
+      <AccordionSummary
+        id="related-videos-header"
+        expandIcon={<ExpandMoreIcon />}
+      >
+        <Typography variant="h5" sx={{ display: "flex", alignItems: "center" }}>
+          <VideoLibraryIcon />
+          &nbsp;&nbsp;Related Videos
+        </Typography>
+      </AccordionSummary>
+      <AccordionDetails>
+        <Grid container spacing={2}>
+          {videos.map((video) => {
+            return (
+              <Grid
+                item
+                xs={12}
+                sm={6}
+                md={4}
+                lg={3}
+                xl={2}
+                key={video.videoId}
+              >
+                <VideoCard
+                  video={{
+                    id: video.videoId,
+                    title: video.title,
+                    imageUrl: video.thumbnail,
+                    channelId: video.channelId,
+                    channelTitle: video.channelTitle,
+                    viewCount: video.viewCount,
+                    publishedAt: video.publishedAt,
+                  }}
+                />
+              </Grid>
+            );
+          })}
+        </Grid>
+      </AccordionDetails>
+    </Accordion>
   );
 }
