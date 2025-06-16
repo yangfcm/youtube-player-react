dddc781d5a0e3543bb7aa90cbe9c785c22a7e6d1
yangfcm <yangfcm@outlook.com>
2025-06-16 23:08:18 +0930

    Remove.

--- src/features/video/useVideo.ts
+++ src/features/video/useVideo.ts
@@ -2,7 +2,7 @@ import { useEffect } from "react";
 import { useSelector } from "react-redux";
 import { useAppDispatch } from "../../app/hooks";
 import { RootState } from "../../app/store";
-import { fetchVideo, fetchVideoInfo } from "./videoSlice";
+import { fetchVideo } from "./videoSlice";
 
 export function useVideo(videoId?: string) {
   const dispatch = useAppDispatch();
