39f4df5277e8e06102ca250c3ed23e04191d34da
yangfcm <yangfcm@outlook.com>
2026-09-12 13:40:56 +0930

    UI for collection details.

--- src/pages/CollectionDetails.tsx
+++ src/pages/CollectionDetails.tsx
@@ -1,10 +1,15 @@
 import { useParams } from "react-router-dom";
+import Box from "@mui/material/Box";
+import Divider from "@mui/material/Divider";
 import Typography from "@mui/material/Typography";
 import { RequireAuth } from "../components/RequireAuth";
 import { RequireLoginPage } from "../components/RequireLoginPage";
 import { ErrorMessage } from "../components/ErrorMessage";
 import { LoadingSpinner } from "../components/LoadingSpinner";
 import { NoContent } from "../components/NoContent";
+import { VideoItem } from "../components/VideoItem";
+import { ChannelItem } from "../components/ChannelItem";
+import { PlayListItem } from "../components/PlayListItem";
 import { useCollection } from "../features/collection/useCollection";
 import { AsyncStatus } from "../settings/types";
 
@@ -16,7 +21,12 @@ export default function CollectionDetails() {
     <RequireAuth unAuthedComponent={<RequireLoginPage />}>
       <ErrorMessage open={status === AsyncStatus.FAIL}>{error}</ErrorMessage>
       <Typography variant="h5" sx={{ mb: 2 }}>
-        Collection Details
+        Collection
+        {collection && (
+          <Typography component="span" variant="h5" color="primary" sx={{ ml: 1 }}>
+            {collection.name}
+          </Typography>
+        )}
       </Typography>
       {status === AsyncStatus.LOADING && <LoadingSpinner />}
       {status === AsyncStatus.SUCCESS && !collection && (
@@ -30,9 +40,52 @@ export default function CollectionDetails() {
       {status === AsyncStatus.SUCCESS &&
         collection &&
         collection.items.length > 0 && (
-          <Typography component="pre">
-            {JSON.stringify(collection, null, 2)}
-          </Typography>
+          <Box
+            sx={{
+              pb: 2,
+              px: {
+                xs: 0,
+                lg: 5,
+              },
+            }}
+          >
+            {collection.items.map((item, index) => (
+              <Box key={`${item.type}-${item.itemId}`}>
+                {item.type === "video" ? (
+                  <VideoItem
+                    video={{
+                      id: item.itemId,
+                      title: item.title,
+                      channelId: item.channelId,
+                      channelTitle: item.channelTitle,
+                      imageUrl: item.imageUrl,
+                    }}
+                  />
+                ) : item.type === "channel" ? (
+                  <ChannelItem
+                    channel={{
+                      id: item.itemId,
+                      title: item.title,
+                      imageUrl: item.imageUrl,
+                    }}
+                  />
+                ) : item.type === "playlist" ? (
+                  <PlayListItem
+                    playlist={{
+                      id: item.itemId,
+                      title: item.title,
+                      imageUrl: item.imageUrl,
+                      channelId: item.channelId,
+                      channelTitle: item.channelTitle,
+                    }}
+                  />
+                ) : null}
+                {index < collection.items.length - 1 && (
+                  <Divider sx={{ my: 1 }} />
+                )}
+              </Box>
+            ))}
+          </Box>
         )}
     </RequireAuth>
   );
